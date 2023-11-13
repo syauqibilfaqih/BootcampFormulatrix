ac1404527be635f2e0da8deb91b804f90684efd6
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2023-11-13 11:47:20 +0700

    Add library Cat

---
+++ Bootcamp/Cat.cs
@@ -0,0 +1,39 @@
+class Cat
+{
+	public string colour;
+	public int age;
+	public bool isMale;
+	public float weight;
+	
+	public Cat(string colour, int age, bool isMale, float weight)
+	{
+		this.colour = colour;
+		this.age = age;
+		this.isMale = isMale;
+		this.weight = weight;
+	}
+	public Cat()
+	
+	{
+		this.colour="No parameter inserted";
+		this.age=0;
+		this.isMale=false;
+		this.weight=0.0F;
+	} 
+	public void Eat()
+	{
+		Console.WriteLine("Awm");
+	} 
+	public void Sleep()
+	{
+		Console.WriteLine("ZZZZZZZ");
+	} 
+	public void Chaos()
+	{
+		Console.WriteLine("KJNDCKJNDSKJNDSK");
+	} 
+	public void Meow()
+	{
+		Console.WriteLine("Meow");
+	} 
+}
--- Bootcamp/Program.cs
+++ Bootcamp/Program.cs
@@ -1,46 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
-class Cat
-{
-	public string colour;
-	public int age;
-	public bool isMale;
-	public float weight;
-	
-	public Cat(string colour, int age, bool isMale, float weight)
-	{
-		this.colour = colour;
-		this.age = age;
-		this.isMale = isMale;
-		this.weight = weight;
-	}
-	public Cat()
-	
-	{
-		this.colour="No parameter inserted";
-		this.age=0;
-		this.isMale=false;
-		this.weight=0.0F;
-	} 
-	public void Eat()
-	{
-		Console.WriteLine("Awm");
-	} 
-	public void Sleep()
-	{
-		Console.WriteLine("ZZZZZZZ");
-	} 
-	public void Chaos()
-	{
-		Console.WriteLine("KJNDCKJNDSKJNDSK");
-	} 
-	public void Meow()
-	{
-		Console.WriteLine("Meow");
-	} 
-}
-
 class Program
 {
 	static void Main()
