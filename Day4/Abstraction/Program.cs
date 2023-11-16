e1ece86beca2b2ae4475203ced1128e8bffc4ed8
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2023-11-16 10:13:15 +0700

    Abstraction

---
+++ Day4/Abstraction/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program {
+	static void Main() {
+		RumahMewah rm = new RumahMewah();
+		RumahBiasa rb = new RumahBiasa();
+		RumahSederhana rs = new RumahSederhana();
+		
+		Rumah rumah = rb;
+		rumah.BukaPintu();
+	}
+}
+abstract class Rumah
+{
+	public readonly int i;
+	public void BukaJendela() {
+		"Buka jendela".Dump();
+	}
+	public virtual void BukaGarasi() {
+		"Keatas".Dump();
+	}
+	public abstract void BukaPintu();
+}
+class RumahMewah : Rumah
+{
+	public override void BukaPintu() {
+		"Keatas".Dump();
+	}
+}
+class RumahBiasa : Rumah
+{
+	public override void BukaPintu()
+	{
+		"Kedepan".Dump();
+	}
+}
+class RumahSederhana : Rumah
+{
+	public override void BukaPintu()
+	{
+		"Gorden".Dump();
+	}
+}
+
+public static class IniExtension
+{
+	public static void Dump(this object x) 
+	{
+		Console.WriteLine(x.ToString());
+	}
+}
