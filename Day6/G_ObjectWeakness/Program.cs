e397fc47007bafc9c9b1c706dd32350818b41978
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2023-11-20 09:36:30 +0700

    ObjectWeakness

---
+++ Day6/G_ObjectWeakness/Program.cs
@@ -0,0 +1,20 @@
+﻿//Object weakness
+//No type check
+using System.Collections;
+
+void Main()
+{
+	//Fixed Size
+	//Not safety
+	object[] myArray = {1,"true",true, 3.0f, 5};
+	
+	//Dynamic Size
+	//Not safety
+	ArrayList myArrayList = new ArrayList();
+	myArrayList.Add(1);
+	myArrayList.Add(true);
+	myArrayList.Add("true");
+	
+	Type type = myArray[0].GetType();
+}
+
