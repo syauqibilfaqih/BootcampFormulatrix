13fec3e9522d02895d84e603252c6b86f540455e
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2023-11-21 10:06:21 +0700

    Add Dump extension

--- Day7/B_Generic2/Program.cs
+++ Day7/B_Generic2/Program.cs
@@ -1,14 +1,16 @@
-﻿void Main()
-{
-	CustomCollection<int> custom = new CustomCollection<int>(20);
-	custom.Add(0, 1);
-	custom.Add(1, 3);
-	//custom.Add(2, "hello");
-	int result = custom.GetValue(1);
-	result.Dump();
-	
-	CustomCollection<string> custom2 = new(2);
-	custom2.Add(0,"a");
+﻿class Program{
+	static void Main()
+	{
+		CustomCollection<int> custom = new CustomCollection<int>(20);
+		custom.Add(0, 1);
+		custom.Add(1, 3);
+		//custom.Add(2, "hello");
+		int result = custom.GetValue(1);
+		result.Dump();
+		
+		CustomCollection<string> custom2 = new(2);
+		custom2.Add(0,"a");
+	}
 }
 class CustomCollection<T>
 {
@@ -25,5 +27,13 @@ class CustomCollection<T>
 	public T GetValue(int index)
 	{
 		return myArray[index];
+	}
+}
+
+public static class IniExtension
+{
+	public static void Dump(this object x) 
+	{
+		Console.WriteLine(x.ToString());
 	}
 }
