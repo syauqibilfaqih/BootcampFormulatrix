0f756054b6028632f5205d800bea9e9303766d40
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2023-11-21 10:23:47 +0700

    Using namespace Dumper

--- Day7/D_OperatorOverloading/Program.cs
+++ Day7/D_OperatorOverloading/Program.cs
@@ -1,12 +1,19 @@
 ﻿//Operator Overloading
-void Main()
+using Dumper;
+class Program
 {
-	Car cara = new(2);
-	Car carb = new(3);
-	Car carc = new(5);
-	Car card = new(5);
-	Car result = cara + carb + carc + card;
+	static void Main()
+	{
+		Car cara = new(2);
+		Car carb = new(3);
+		Car carc = new(5);
+		Car card = new(5);
+		Car result = cara + carb + carc + card;
+		
+		result.price.Dump();
+	}
 }
+
 class Car
 {
 	public int price;
