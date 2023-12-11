67e733703965ddf8fd0f84454b4885a12a9650b4
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2023-12-11 09:17:24 +0700

    Added some comments

--- Day18/P_AsyncAwait/Program.cs
+++ Day18/P_AsyncAwait/Program.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿using System.Threading.Tasks.Sources;
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using System;
 using System.Threading.Tasks;
 
 class Program
 {
-    static async Task Main()
-    {
-        Console.WriteLine("Starting the operation...");
-        await DoWorkAsync();
-        Console.WriteLine("Operation completed.");
-    }
+	static async Task Main()
+	{
+		Console.WriteLine("Starting the operation...");
+		await DoWorkAsync(); // try without await
+		Console.WriteLine("Operation completed.");
+		// await Task.Delay(3000);
+	}
 
-    static async Task DoWorkAsync()
-    {
-        Console.WriteLine("Starting the asynchronous work...");
-        await Task.Delay(2000); // Simulates an async operation taking 2 seconds
-        Console.WriteLine("Asynchronous work completed.");
-    }
+	static async Task DoWorkAsync()
+	{
+		Console.WriteLine("Starting the asynchronous work...");
+		await Task.Delay(2000); // Simulates an async operation taking 2 seconds
+		Console.WriteLine("Asynchronous work completed.");
+	}
 }
