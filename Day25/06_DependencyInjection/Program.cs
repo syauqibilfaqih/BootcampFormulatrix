8b1c61a0960923678f6e48104e4cdbe842b872e4
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2023-12-21 08:38:22 +0700

    Added simple Dependency Injection

---
+++ Day25/06_DependencyInjection/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+void Main()
+{
+	IServiceCollection service = new ServiceCollection();
+	service.AddTransient<IBoard, Board>();
+	service.AddTransient<GameController>();
+	IServiceProvider provider = service.BuildServiceProvider();
+	
+	GameController game = provider.GetRequiredService<GameController>();
+}
+
+class GameController {
+	private IBoard _board;
+	public GameController(IBoard board) {
+		_board = board;
+	}
+}
+interface IBoard { }
+class Board : IBoard { }
